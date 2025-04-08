d41bde729cefbe59c79532925bcebef7ba150dc5
cristina reina <angela.liscano@escola.pr.gov.br>
2025-04-08 19:27:04 -0300

    Delete gabriel-main/SQL g directory

--- gabriel-main/SQL g/SQLite.1.sql
+++
@@ -1,7 +0,0 @@
-insert into agv (nome, turma, idade)
-VALUES ('cristina', '3ds', 17),
-('mirella', '3ds', 17),
-('sofia', '3ds', 16),
-('isis', '3ds', 17),
-('jhonata', '3ds', 17),
-('gabriel', '3ds', 25);
--- gabriel-main/SQL g/SQLite.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE agv
-(id integer PRIMARY KEY,
- nome text not null,
- turma text NOT null ,
- idade integer not null);
