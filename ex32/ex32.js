c3b50d47a6ea0eeb729e9bf6b55f3d698ee5cdfd
cristina reina <angela.liscano@escola.pr.gov.br>
2025-05-08 12:29:42 -0300

    Add files via upload

---
+++ ex32/ex32.js
@@ -0,0 +1,7 @@
+function adicionarTarefa(){
+    const input= document.getElementById('nova-tarefa');
+    const textoTarefa= input.values.trim();
+
+}
+if (textoTarefa==="")return;
+// paramos na linha 71//
